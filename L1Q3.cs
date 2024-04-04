61687a12c0e366024f0e973e27d6de7ae6bb9969
kasmalshini <135606482+kasmalshini@users.noreply.github.com>
2024-04-04 14:40:21 +0530

    Add files via upload

---
+++ L1Q3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number we want to find the sum of all numbers until");
+            int num = int.Parse(Console.ReadLine());
+
+            int tot = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                tot = tot + i;
+            }
+
+            Console.WriteLine($"Sum is {tot}");
+            Console.ReadLine();
+        }
+    }
+}
