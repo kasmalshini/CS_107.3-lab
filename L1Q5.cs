217c9736061611710818b0c27207a5d692405ab0
kasmalshini <135606482+kasmalshini@users.noreply.github.com>
2024-04-04 14:45:52 +0530

    Add files via upload

---
+++ L1Q5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace mtable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to get the multiplication table");
+            int num = int.Parse(Console.ReadLine());
+
+            int[,] nset = new int[num + 1, num + 1];
+
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= num; j++)
+                {
+                    nset[i, j] = i * j;
+                }
+            }
+
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= num; j++)
+                {
+                    Console.WriteLine($"{i}*{j}={nset[i, j]}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
