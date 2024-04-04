ed1ad588452c06111d92fd1c5ee544974a0d01d7
kasmalshini <135606482+kasmalshini@users.noreply.github.com>
2024-04-04 14:56:07 +0530

    Add files via upload

---
+++ program1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q4
+{
+    class Product
+    {
+        public string ProductName;
+        public int Price;
+
+        public Product(string productname, int price)
+        {
+            ProductName = productname;
+            Price = price;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product("mars", 450);
+
+            Console.WriteLine($"Product Name : {product1.ProductName} \nPrice : {product1.Price}");
+            Console.ReadLine();
+        }
+    }
+}
