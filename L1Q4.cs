cdd5a0c1ef9286a0e2bc48c93814743624469de6
kasmalshini <135606482+kasmalshini@users.noreply.github.com>
2024-04-04 14:43:31 +0530

    Add files via upload

---
+++ L1Q4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter upto which fibonacci number you want to display");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("fibonacci series displays below");
+            for (int i = 0; i <= num; i++)
+            {
+                int result = fibo(i);
+                Console.WriteLine(result);
+
+            }
+
+            Console.ReadLine();
+        }
+        static int fibo(int i)
+        {
+            if (i <= 1)
+            {
+                return i;
+            }
+            else
+            {
+                return fibo(i - 1) + fibo(i - 2);
+            }
+        }
+
+    }
+}
